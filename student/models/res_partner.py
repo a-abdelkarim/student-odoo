7920730283d491dee834283b357b36f856817716
Ahmed Abdelkarim <=>
2023-08-28 20:21:22 +0300

    create birth_date field

--- student/models/res_partner.py
+++ student/models/res_partner.py
@@ -4,3 +4,4 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     is_student = fields.Boolean(string='Is Student', default=False)
+    birth_date = fields.Date(string='Birth Date')
