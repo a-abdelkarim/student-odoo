4cf2ff105a883a906dc18d873478a067ab2dfa9f
Ahmed Abdelkarim <=>
2023-08-28 20:59:52 +0300

    students filter

--- student/__manifest__.py
+++ student/__manifest__.py
@@ -27,6 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/filters.xml',
         'views/res_partner.xml',
     ],
     # only loaded in demonstration mode
